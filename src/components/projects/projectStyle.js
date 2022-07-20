d5d6a0cfcb7f1c02333c7d7d48ec70f43abcd5ee
roji-tech <rojitech9@gmail.com>
2022-07-20 02:30:33 +0100

    added

--- src/components/projects/projectStyle.js
+++ src/components/projects/projectStyle.js
@@ -26,7 +26,7 @@ export const ProjectStyle = styled.section`
 `;
 
 export const ProjectBoxStyle = styled.div`
-  width: 300px;
+  width: 310px;
   height: 450px;
   border-top-left-radius: 30px;
   border-top-right-radius: 30px;
